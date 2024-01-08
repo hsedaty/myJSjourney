d3680cf254e67edc672f51237f5eb132f3ada229
hsedaty <hsedaty@gmail.com>
2024-01-08 18:38:06 +0200

    - interactive buttons added - much better look - ready for submission

--- aboutMe.js
+++ aboutMe.js
@@ -1,3 +1,14 @@
 function redirectToPage(page) {
     window.location.href = page
 }
+
+var abtMeButton = document.getElementById('aboutMeButton');
+var dispText = document.getElementById('displayedText');
+
+abtMeButton.addEventListener("click", function() {
+    if (dispText.style.display === "none") {
+        dispText.style.display = "block";
+    } else {
+        dispText.style.display = "none";
+    }
+});
--- journey.js
+++ journey.js
@@ -1,3 +1,64 @@
 function redirectToPage(page) {
     window.location.href = page
 }
+
+// "START"
+var start_button = document.getElementById('startButton');
+var start_txt = document.getElementById('startTxt');
+var start_img = document.getElementById('startImg');
+
+start_button.addEventListener("click", function () {
+    if (start_txt.style.display === "none" && start_img.style.display === "none"){
+        start_txt.style.display = "block";
+        start_img.style.display = "block";
+    } else {
+        start_txt.style.display = "none";
+        start_img.style.display = "none";
+    }
+
+});
+
+// "VISUALS"
+var visuals_button = document.getElementById('visualsButton');
+var visuals_txt = document.getElementById('visualsTxt');
+var visuals_img = document.getElementById('visualsImg');
+
+visuals_button.addEventListener("click", function () {
+    if (visuals_txt.style.display === "none" && visuals_img.style.display === "none") {
+        visuals_txt.style.display = "block";
+        visuals_img.style.display = "block";
+    } else {
+        visuals_txt.style.display = "none";
+        visuals_img.style.display = "none";
+    }
+});
+
+// "PLANNING"
+var planning_button = document.getElementById('planningButton');
+var planning_txt = document.getElementById('planningTxt');
+var planning_img = document.getElementById('planningImg');
+
+planning_button.addEventListener("click", function () {
+    if (planning_txt.style.display === "none" && planning_img.style.display === "none") {
+        planning_txt.style.display = "block";
+        planning_img.style.display = "block";
+    } else {
+        planning_txt.style.display = "none";
+        planning_img.style.display = "none";
+    }
+});
+
+// "SUMMARY"
+var summary_button = document.getElementById('summaryButton');
+var summary_txt = document.getElementById('summaryTxt');
+var summary_img = document.getElementById('summaryImg');
+
+summary_button.addEventListener("click", function () {
+    if (summary_txt.style.display === "none" && summary_img.style.display === "none") {
+        summary_txt.style.display = "block";
+        summary_img.style.display = "block";
+    } else {
+        summary_txt.style.display = "none";
+        summary_img.style.display = "none";
+    }
+});
